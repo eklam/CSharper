a61dca516eb1f669901a06018baeac08cd8812b1
unknown <renan.stigliani@ITLNB027.itlab.local>
2013-07-17 14:02:12 -0300

    Created solution, StringSharper and IEnumerableSharper

---
+++ CSharper/IEnumerableSharper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharper
+{
+    public static class IEnumerableSharper
+    {
+        public static IEnumerable<T> Safe<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable ?? new T[0];
+        }
+
+        public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || enumerable.Count() == 0 ? true : false;
+        }
+
+        public static void SafeForEach<T>(this IEnumerable<T> enumerable, Action<T> act)
+        {
+            if (enumerable != null)
+            {
+                foreach (T item in enumerable)
+                {
+                    act(item);
+                }
+            }
+        }
+    }
+}
