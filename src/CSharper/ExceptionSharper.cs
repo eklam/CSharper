123ba3ec71ab42cdd0894dd01b12b16e05244593
unknown <renan.stigliani@ITLNB027.itlab.local>
2013-07-18 08:05:32 -0300

    Added ExceptioSharper and Tests

---
+++ src/CSharper/ExceptionSharper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharper
+{
+    public static class ExceptionSharper
+    {
+        /// <summary>
+        /// Throws an exception if the object called upon is null.
+        /// </summary>
+        /// <typeparam name="T">The calling class</typeparam>
+        /// <param name="obj">The This object</param>
+        /// <param name="text">The text to be written on the ArgumentNullException: [text] not allowed to be null</param>
+        public static void ThrowIfArgumentIsNull<T>(this T obj, string text) where T : class
+        {
+            if (obj == null)
+                throw new ArgumentNullException(text + " not allowed to be null");
+        }
+    }
+}
