5e8a06e0ca25e1896dcb3b8075629b7e465bd2ee
Renan Stigliani <renan.stigliani@ITLNB027.itlab.local>
2013-07-19 16:38:32 -0300

    Added dateTimeSharper.IsWeekend and related tests

---
+++ src/CSharper.Tests/DateTime/DateTimeTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharper;
+
+namespace CSharper.Tests
+{
+    [TestClass]
+    public class DateTimeTest
+    {
+        [TestMethod]
+        public void IsWeekendWithWeekendDate()
+        {
+            var date = new DateTime(2013, 07, 20);
+
+            Assert.IsTrue(date.IsWeekend());
+        }
+
+        [TestMethod]
+        public void IsWeekendWithWeekendDate2()
+        {
+            var date = new DateTime(1990, 07, 15);
+
+            Assert.IsTrue(date.IsWeekend());
+        }
+
+        [TestMethod]
+        public void IsWeekendWithWeekendDate3()
+        {
+            var date = new DateTime(2030, 06, 16);
+
+            Assert.IsTrue(date.IsWeekend());
+        }
+
+        [TestMethod]
+        public void IsWeekendWithNonWeekendDate()
+        {
+            var date = new DateTime(2013, 07, 19);
+
+            Assert.IsFalse(date.IsWeekend());
+        }
+
+        [TestMethod]
+        public void IsWeekendWithNonWeekendDate2()
+        {
+            var date = new DateTime(1990, 07, 12);
+
+            Assert.IsFalse(date.IsWeekend());
+        }
+
+        [TestMethod]
+        public void IsWeekendWithNonWeekendDate3()
+        {
+            var date = new DateTime(2030, 06, 13);
+
+            Assert.IsFalse(date.IsWeekend());
+        }
+    }
+}
---
+++ src/CSharper/DateTimeSharper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharper
+{
+    public static class DateTimeSharper
+    {
+        /// <summary>
+        /// Lets you easily figure out ifdateTime holds a date value that is a weekend.
+        /// </summary>
+        /// <param name="date">The date to be verified</param>
+        /// <returns>true if the date is Saturday or Sunday; false otherwise</returns>
+        /// <see cref="http://extensionmethod.net/csharp/datetime/isweekend"/>
+        public static bool IsWeekend(this DateTime date)
+        {
+            return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+        }
+    }
+}
