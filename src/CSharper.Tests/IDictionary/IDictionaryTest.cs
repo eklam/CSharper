1f7b317da0ac2695e1fe6ecd9cc3f373986ce87f
Renan Stigliani <renan.stigliani@ITLNB027.itlab.local>
2013-07-23 09:48:20 -0300

    Added IDictionary.GetValueOrDefault and related tests

---
+++ src/CSharper.Tests/IDictionary/IDictionaryTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharper.Tests.IDictionary
+{
+    [TestClass]
+    public class IDictionaryTest
+    {
+        [TestMethod]
+        public void GetValueOrDefaultValidKey()
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            dict.Add(1, 1);
+            dict.Add(2, 2);
+            dict.Add(3, 3);
+
+            Assert.AreEqual(1, dict.GetValueOrDefault(1), "IDictionarySharper ain't working properly!");
+        }
+
+        [TestMethod]
+        public void GetValueOrDefaultValidKey2()
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            dict.Add(1, 1);
+            dict.Add(2, 2);
+            dict.Add(3, 3);
+
+            Assert.AreEqual(2, dict.GetValueOrDefault(2), "IDictionarySharper ain't working properly!");
+        }
+
+        [TestMethod]
+        public void GetValueOrDefaultInvalidKey()
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            dict.Add(1, 1);
+            dict.Add(2, 2);
+            dict.Add(3, 3);
+
+            Assert.AreEqual(0, dict.GetValueOrDefault(4), "IDictionarySharper ain't working properly!");
+        }
+
+        [TestMethod]
+        public void GetValueOrDefaultInvalidKeyAndDefault()
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            dict.Add(1, 1);
+            dict.Add(2, 2);
+            dict.Add(3, 3);
+
+            Assert.AreEqual(-1, dict.GetValueOrDefault(4, -1), "IDictionarySharper ain't working properly!");
+        }
+    }
+}
---
+++ src/CSharper/IDictionarySharper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharper
+{
+    public static class IDictionarySharper
+    {
+        /// <summary>
+        /// Gets the value associated with the specified key, or default value, if key not present
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dict">The dictionary in wich the method is going to be executed on</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <param name="defValue">The value to be returne when the key is not found</param>
+        /// <returns>the value associated with the specified key, if the key is found; otherwise, the default value passed as parameter</returns>
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IDictionary<TKey, TValue> dict,
+            TKey key,
+            TValue defValue = default(TValue))
+        {
+            TValue outValue;
+            if (dict.TryGetValue(key, out outValue))
+            {
+                return outValue;
+            }
+
+            return defValue;
+        }
+    }
+}
