6d284d84c817bc88a3e5d2118ed13e19c44cabc3
eklam <malke.eklam@gmail.com>
2013-09-30 10:11:47 -0300

    Added IListSharper 2

---
+++ src/CSharper/IListSharper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharper
+{
+    public static class IListSharper
+    {
+        public static void AddRange<T>(this IList<T> source, IEnumerable<T> items)
+        {
+            if (source == null)
+                throw new ArgumentNullException("Source list cannot be null.");
+
+            foreach (var item in items)
+            {
+                source.Add(item);
+            }
+        }
+    }
+}
