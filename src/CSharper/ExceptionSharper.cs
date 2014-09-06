7e74ecd305adc03eaaeed14a6b4bd94aa96d7874
rmalke <malke.eklam@gmail.com>
2014-09-05 23:22:18 -0300

    Adding ThrowIfEmpty to ExceptionSharper

--- src/CSharper/ExceptionSharper.cs
+++ src/CSharper/ExceptionSharper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace CSharper
 {
@@ -27,6 +29,30 @@ namespace CSharper
         {
             if (obj == null)
                 throw new ArgumentNullException(paramName, message);
+        }
+
+        /// <summary>
+        /// Throws an exception if the IEnumerable is Empty
+        /// </summary>
+        /// <typeparam name="T">The calling class</typeparam>
+        /// <param name="obj">The This object</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        public static void ThrowIfIsEmpty<T>(this IEnumerable<T> obj, string paramName) where T : class
+        {
+            obj.ThrowIfIsEmpty(paramName, "The argument must be a non-empty IEnumerable");
+        }
+
+        /// <summary>
+        /// Throws an exception if the IEnumerable is Empty
+        /// </summary>
+        /// <typeparam name="T">The calling class</typeparam>
+        /// <param name="obj">The This object</param>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        public static void ThrowIfIsEmpty<T>(this IEnumerable<T> obj, string paramName, string message) where T : class
+        {
+            obj.ThrowIfArgIsNull(paramName);
+            if (obj.Count() == 0)
+                throw new ArgumentException(message, paramName);
         }
     }
 }
